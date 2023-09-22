dd2f72e21696ac0205693905684f6a5a20ae3fe8
Nathaniel Barrera <nathaniel.barrera39@gmail.com>
2023-09-22 00:40:04 -0500

    Added some pages and styling

--- weekFour/projectOne/wiki/views.py
+++ weekFour/projectOne/wiki/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponseRedirect
+from django import forms
 from django.shortcuts import render
+from django.urls import reverse
+
+
+entries = []
 
 # Create your views here.
+def index(request):
+    return render(request, "wiki/index.html")
+
+def entry(request):
+    return render(request, "wiki/entry.html")
+
